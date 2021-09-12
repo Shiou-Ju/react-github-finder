fcf20f224e178277cddb90c684d75f08464f0acd
Shiou-Ju <75402531+Shiou-Ju@users.noreply.github.com>
2021-09-12 16:03:17 +0800

    User Item using state with constructor
    - no need for constructor actually.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Navbar from "./components/layout/Navbar";
+import UserItem from "./components/users/UserItem.js";
 import "./App.css";
 
 class App extends Component {
@@ -7,6 +8,7 @@ class App extends Component {
     return (
       <div className="App">
         <Navbar title="Github用戶 搜尋器" icon="fab fa-github" />
+        <UserItem />
       </div>
     );
   }
