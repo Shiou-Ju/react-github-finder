9a3c2d84e894545ff99024031268deadb38f5b48
Shiou-Ju <75402531+Shiou-Ju@users.noreply.github.com>
2021-09-12 17:08:27 +0800

    Fetch Github api and render

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import React, { Component } from "react";
 import Navbar from "./components/layout/Navbar";
 import Users from "./components/users/Users.js";
+import axios from "axios";
 import "./App.css";
 
 class App extends Component {
+  state = {
+    users: [],
+    // while fetching it should be true
+    loading: false,
+  };
+
+  async componentDidMount() {
+    this.setState({ loading: true });
+    const res = await axios.get(`https://api.github.com/users`);
+    this.setState({ loading: false, users: res.data });
+  }
+
   render() {
     return (
       <div className="App">
         <Navbar title="Github用戶 搜尋器" icon="fab fa-github" />
         <div className="container">
-          <Users />
+          <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
     );
