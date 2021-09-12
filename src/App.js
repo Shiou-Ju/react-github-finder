08644b3a3523270e692f16ef13a825f261e33ac5
Shiou-Ju <75402531+Shiou-Ju@users.noreply.github.com>
2021-09-12 16:16:37 +0800

    Add UserItem to display single user and Users to be the main container

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Navbar from "./components/layout/Navbar";
 import UserItem from "./components/users/UserItem.js";
+import Users from "./components/users/Users.js";
 import "./App.css";
 
 class App extends Component {
@@ -8,7 +9,7 @@ class App extends Component {
     return (
       <div className="App">
         <Navbar title="Github用戶 搜尋器" icon="fab fa-github" />
-        <UserItem />
+        <Users />
       </div>
     );
   }
