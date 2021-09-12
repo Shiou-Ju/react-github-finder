c1e7f3272e4fa83305794b5408d77479a35554aa
Shiou-Ju <75402531+Shiou-Ju@users.noreply.github.com>
2021-09-12 15:14:02 +0800

    Expressions and using class methods

--- src/App.js
+++ src/App.js
@@ -2,18 +2,16 @@ import React, { Fragment, Component } from "react";
 import "./App.css";
 
 class App extends Component {
-  render() {
-    return React.createElement(
-      `div`,
-      { className: "app" },
-      React.createElement(`h1`, null, "hello from react...")
-    );
+  foo = () => `bars`;
 
-    // (
-    //   <div className="App">
-    //     <h1>hello from React</h1>
-    //   </div>
-    // );
+  render() {
+    const name = `Bamboo Lin`;
+
+    return (
+      <div className="App">
+        <h1>hello {this.foo()}</h1>
+      </div>
+    );
   }
 }
 
